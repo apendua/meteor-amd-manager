0e1d3149c4c4df0f08cabc1d56cea544fc52453f
apendua <apendua@gmail.com>
2014-07-26 14:52:57 +0200

    Added some mocha tests

--- tests/manager.js
+++ tests/manager.js
@@ -8,4 +8,122 @@ describe('AMDManager', function () {
     expect(manager).to.be.ok;
   });
 
+  describe('define/require', function (test) {
+
+    var manager = new AMDManager(),
+        require = manager.require,
+        define  = manager.define;
+
+    before(function () {
+      require(['a', 'b'], function (a, b) {
+        expect(a).to.equal(1);
+        expect(b).to.equal(2);
+      });
+    });
+
+    it('should be able to define modules', function () {
+
+      define('b', ['a', ], function () {
+        return 2;
+      });
+
+      define('a', [], function () {
+        return 1;
+      });
+
+    });
+
+  });
+
+
+  describe('relative paths', function (test) {
+
+    var manager = new AMDManager(),
+        require = manager.require,
+        define  = manager.define,
+        loaded  = false;
+
+    define('path/to/b', ['./a', '../c', '../../path/to2/../to2/e'], function (a, c, e) {
+      return a + c + e; // 6
+    });
+
+    define('path/to/a', [], function () {
+      return 1;
+    });
+
+    define('path/c', ['./to/a'], function (a) {
+      return 1 + 2 * a; // 3
+    });
+
+    define('path/to2/d', ['../to/b'], function (b) {
+      return 4 * b + 1; // 25
+    });
+
+    define('path/to2/e', ['../to/a'], function (a) {
+      return a + 1; // 2
+    });
+
+    it('should be able to use relative paths', function (done) {
+      require(['path/to2/d'], function (d) {
+        expect(d).to.equal(25);
+        done();
+      });
+    })
+
+  });
+
+  describe('module not found', function (test) {
+
+    var manager = new AMDManager({
+          onModuleNotFound: function () {
+            notFound++;
+          },
+        }),
+        require  = manager.require,
+        define   = manager.define,
+        notFound = 0;
+
+    manager.onModuleNotFound(function () {
+      notFound++;
+    });
+
+    require(['a', 'b', 'c']);
+
+    define('a', ['b', 'c'], function () {
+
+    });
+
+    it('should be able to use onModuleNotFound hook', function () {
+      expect(notFound).to.equal(10);
+    });
+
+  });
+
+  describe('circular dependencies', function (test) {
+
+    var manager = new AMDManager(),
+        require = manager.require,
+        define  = manager.define,
+        loaded  = false;
+
+    define('a', ['b'], function () {
+
+    });
+
+    define('b', ['c'], function () {
+
+    });
+
+    define('c', ['a'], function () {
+
+    });
+
+    it('module with circular dependencies should not load', function () {
+      require('a', function () {
+        throw new Error('module "a" was loaded');
+      });
+    });
+
+  });
+
 });
