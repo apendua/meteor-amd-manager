7059408ddb05406ccd6f815451985214fbae286e
apendua <apendua@gmail.com>
2014-08-27 09:28:33 +0200

    Updated to 0.9.0

--- package.js
+++ package.js
@@ -1,9 +1,13 @@
 
 Package.describe({
   summary: "A simple utility class for creating AMD module managers",
+  version: "0.0.4",
+  name: "apendua:amd-manager",
+  git: "https://github.com/apendua/meteor-amd-manager.git",
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use(['underscore'], ['client', 'server']);
   
   api.add_files([
